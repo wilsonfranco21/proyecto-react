89f9a0b7295ba28971c9a757686f3bdd7b354884
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 15:49:40 -0500

    finalizado

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -11,37 +11,36 @@ export function Home() {
     return (
         <>
 
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="../assets/imagenes/logo baner.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-      Bootstrap
-    </a>
-  </div>
-</nav>
+            <div className="bg-primary border-bottom border-3 border-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                    <img src="../../src/assets/logo baner.png" alt="Logo" width="175" height="75" className="img-fluid d-inline-block align-text-top my-2"/>
+                    </a>
+                </div>
+            </div>
 
-            <div className="container m-5">
-                <div className="row">
-                    <div className="col-6 col-md-6">
-                        <h3 className="fuente">Módulo Asignación de Empresas</h3>
+            <div className="container-fluid mt-5 pb-3 pt-2">
+                <div className="row d-flex d-flex justify-content-center">
+                    <div className="col-6 col-md-6 me-5">
+                        <h3 className="fuente mb-4">Módulo Asignación de Empresas</h3>
                         <h2 className="text-muted">Bienvenido <span className="fuente">{datos?.nombreUsuario}</span></h2>
-                        <p>A continuación encontrarás información sobre las empresas asignadas a cada Consultor, esto te permitirá gestionar el proyecto de gestión de riesgo y visualizar los consultores asignados a cada empresa.</p>
+                        <p className="">A continuación encontrarás información sobre las empresas asignadas a cada Consultor, esto te permitirá gestionar el proyecto de gestión de riesgo y visualizar los consultores asignados a cada empresa.</p>
                     </div>
 
-                    <div className="col-6 col-md-6 text-center card">
-                        <h5>Cargo: <span className="fuente">{datos?.cargo}</span></h5>
-                        <h5>Profesión: <span className="fuente">{datos?.profesion}</span></h5>
-                        <h5>Ruta: <span className="fuente">{datos?.ruta}</span></h5>
+                    <div className="col-6 col-md-6 text-center card w-25 shadow-lg h-100 bg-primary-subtle py-4 bg-primary border border-dark border-3">
+                        <h5 className="fw-bold">Cargo: <span className="fuente fw-normal">{datos?.cargo}</span></h5>
+                        <h5 className="fw-bold">Profesión: <span className="fuente fw-normal">{datos?.profesion}</span></h5>
+                        <h5 className="fw-bold">Ruta: <span className="fuente fw-normal">{datos?.ruta}</span></h5>
                     </div>
                 </div>
             </div>
 
-            <section className="container my-5">
-                <div className="row row-cols-1 row-cols-md-3 g-2">
+            <section className="container d-flex justify-content-evenly mb-5">
+                <div className="row row-cols-12 row-cols-md-3">
                     {datos?.empresas?.map(function(empresa, index) {
                         return (
                             <div className="col m-2" key={index}>
-                                <div className="card h-100 shadow mt-3 text-center pt-3">
+                                <div className="card h-100 shadow mt-3 text-center pt-3 bg-primary border border-dark border-2 text-white">
                                     <h4>{empresa}</h4>
                                 </div>
                             </div>
